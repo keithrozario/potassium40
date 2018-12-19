6383a72f763e13617ecdbc01e2768b17a8f5149b
Keith Rozario <keith@keithrozario.com>
2018-12-20 00:20:35 +0800

    import botocore exceptions

--- invocations.py
+++ invocations.py
@@ -7,6 +7,7 @@ import os
 import datetime
 import concurrent.futures
 import base64
+from botocore.exceptions import ClientError
 
 configuration_file = 'lambda/serverless.yml'
 status_file = 'lambda/deploy/status.json'
@@ -207,6 +208,7 @@ def async_in_region(function_name, payloads, region_name=False, max_workers=1, s
         region_name = get_regions()['region']
 
     lambda_client = boto3.client('lambda', region_name=region_name)
+
     print("Invoking Lambdas in {}".format(region_name))
     start_time = int(datetime.datetime.now().timestamp() * 1000)  # Epoch Time
 
