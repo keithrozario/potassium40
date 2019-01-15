330d571f853ee0ac8706c6597fa0a0605e3204c7
Keith Rozario <keith@keithrozario.com>
2019-01-15 22:00:35 +0800

    added check_lambda

--- get_robots.py
+++ get_robots.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--num_invocations",
                         help="Number of lambdas to invoke, default is 100",
-                        default=100)
+                        default=10)
     parser.add_argument("-p", "--per_lambda",
                         help="Number of records to process per lambda, default is 1250",
-                        default=1250)
+                        default=2)
     parser.add_argument("-m", "--multiproc_count",
                         help="Number of multi-processes per lambda, default is 125",
-                        default=125)
+                        default=2)
 
     args = parser.parse_args()
 
@@ -37,8 +37,7 @@ if __name__ == '__main__':
     _start = time.time()
     invocations.async_in_region(function_name='potassium40-functions-get_robots',
                                 payloads=payloads,
-                                max_workers=4,
-                                sleep_time=5)
+                                sleep_time=10)
 
     _end = time.time()
     print("Time Taken to process {:,} urls is {}s".format(total_urls,
@@ -46,8 +45,7 @@ if __name__ == '__main__':
 
     invocations.async_in_region(function_name='potassium40-functions-compress_bucket',
                                 payloads=[{}],  # no arguments needed
-                                max_workers=1,
-                                sleep_time=5)
+                                sleep_time=10)
 
     print("Time Taken to compress {:,} urls is {}s".format(total_urls,
                                                            time.time() - _start))
